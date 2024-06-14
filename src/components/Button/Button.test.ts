042ab2cd967455992e5ec2e7f2b25660e9f8edb3
Adam Johnson <adamjohnson@Adams-MacBook-Pro-2.local>
2024-06-14 17:56:40 +0100

    Added and configured Vitest

---
+++ src/components/Button/Button.test.ts
@@ -0,0 +1,5 @@
+describe('Placeholder Test', () => {
+  test('Test to prove vitest is working', () => {
+    expect(true).toEqual(true);
+  });
+});
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,5 @@
+const Button = (): JSX.Element => {
+  return <button>Click</button>;
+};
+
+export default Button;
